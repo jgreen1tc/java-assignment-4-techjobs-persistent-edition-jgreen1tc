789151ad9858749fdf0468a4452e0aa4c64839c9
jgreen1tc <contact@jgreen.io>
2022-02-25 12:45:53 -0600

    updated Skill class, data access interfaces

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Max(255)
+    private String description;
+
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
