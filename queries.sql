1198e4ede112e778df48e98fa237621e7ec6aec6
jgreen1tc <contact@jgreen.io>
2022-03-03 20:47:54 -0600

    add last sql sanity check

--- queries.sql
+++ queries.sql
@@ -8,3 +8,6 @@ WHERE location = 'St. Louis City';
 -- Part 3: Test it with SQL
 DROP TABLE `techjobs`.`job`;
 -- Part 4: Test it with SQL
+SELECT name, description
+FROM techjobs.skill
+INNER JOIN techjobs.job_skills ON id = skills_id;
