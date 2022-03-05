4cc5eaddaf9d3c731508b9f703db371cfc592e32
jgreen1tc <contact@jgreen.io>
2022-03-05 08:29:57 -0600

    add IS NOT NULL statement to sql sanity check

--- queries.sql
+++ queries.sql
@@ -1,14 +1,15 @@
 -- Part 1: Test it with SQL
 SELECT *
-FROM techjobs.job;
+FROM job;
 -- Part 2: Test it with SQL
 SELECT name
-FROM techjobs.employer
+FROM employer
 WHERE location = 'St. Louis City';
 -- Part 3: Test it with SQL
 DROP TABLE `techjobs`.`job`;
 -- Part 4: Test it with SQL
 SELECT name, description
-FROM techjobs.skill
-INNER JOIN techjobs.job_skills ON id = skills_id
+FROM skill
+INNER JOIN job_skills ON id = skills_id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name;
